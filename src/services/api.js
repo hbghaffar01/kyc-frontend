059d5edf9b96a77504d85d1b72f7feb63a89dc5b
hbghaffar01 <hbghaffar01@gmail.com>
2025-01-13 21:25:39 +0500

    feat:endpoint-update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, getRefreshToken } from "@/utils/token";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3333",
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
   headers: {
     "Content-Type": "application/json",
   },
